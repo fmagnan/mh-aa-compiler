eb231dd955381a1b9256a5b98405c40a5b65e71e
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-09-28 16:17:24 +0000

    création du compilateur

--- trunk/temp_aa/tests/TousLesTests.php
+++ trunk/temp_aa/tests/TousLesTests.php
@@ -2,8 +2,9 @@
 	class TousLesTests extends GroupTest {
 		function TousLesTests() {
         	parent::GroupTest('');
-        	$this->addTestFile(dirname(__FILE__).'/TestTroll.php');
         	$this->addTestFile(dirname(__FILE__).'/TestAccesBase.php');
+        	$this->addTestFile(dirname(__FILE__).'/TestTroll.php');
+        	$this->addTestFile(dirname(__FILE__).'/TestCompiler.php');
         }
 	}
 ?>
