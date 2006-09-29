5f33588a95f6b767bd1fa482bdf5b04eee6fd73d
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-09-29 15:58:46 +0000

    compilation des données

---
+++ trunk/temp_aa/lib/Compiler.php
@@ -0,0 +1,70 @@
+<?php
+
+class Compiler {
+	
+	var $minimumValue;
+	var $maximumValue;
+	
+	function Compiler($inputString) {
+		$values = $this->getValuesFromString($inputString);
+		$this->minimumValue = $values['minimum'];
+		$this->maximumValue = $values['maximum'];
+	}
+	
+	function getValuesFromString($inputString) {
+		$values = array();
+		if (ereg('^entre', $inputString)) {
+			$endOfString = preg_replace('/entre/', '', $inputString);
+			$valuesArray = explode('et', $endOfString);
+			$values['minimum'] = intval(trim($valuesArray[0]));
+			$values['maximum'] = intval(trim($valuesArray[1]));
+		}
+		elseif ($uniqueValue = $this->checkIfStringContainsPatternAndEraseIt('inférieur à', $inputString)) {}
+		elseif ($uniqueValue = $this->checkIfStringContainsPatternAndEraseIt('supérieur à', $inputString)) {}
+		elseif (($uniqueValue = intval(trim($inputString))) != 0) {}
+		else {
+			trigger_error('error[Compiler()]: wrong input data');
+		}
+		
+		if ($uniqueValue) {
+			$values['minimum'] = intval($uniqueValue);
+			$values['maximum'] = intval($uniqueValue);
+		}
+		return $values;
+	}
+	
+	function checkIfStringContainsPatternAndEraseIt($pattern, $referenceString) {
+		$result = FALSE;
+		if (ereg('^'.$pattern, $referenceString)) {
+			$result = trim(preg_replace('/'.$pattern.'/','',$referenceString));
+		}
+		return $result;
+	}
+	
+	function analyse($newData) {
+		$newValues = $this->getValuesFromString($newData);
+		if ($newValues['maximum'] < $this->getMinimumValue()) {
+			trigger_error('error[Compiler->analyse()]: wrong input data');
+		}
+		else {
+			$referenceMinimumValue = $this->getMinimumValue();
+			$newMinimumValue = $newValues['minimum'];
+			$referenceMaximumValue = $this->getMaximumValue();
+			$newMaximumValue = $newValues['maximum'];
+		
+			if ($newMinimumValue > $referenceMinimumValue) {
+				$this->minimumValue = $newMinimumValue;
+			}
+			$this->maximumValue = $newMaximumValue;
+		}
+	}
+	
+	function getMinimumValue() {
+		return $this->minimumValue;
+	}
+	
+	function getMaximumValue() {
+		return $this->maximumValue;
+	}
+}
+?>
