03cf520e76fa3ac856ab82929942bbfb80fcd4cf
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-09-29 15:58:05 +0000

    modification pour ne tester que les requetes

--- temp_aa/tests/TestAccesBase.php
+++ temp_aa/tests/TestAccesBase.php
@@ -3,100 +3,60 @@
 
 	class TestAccesBase extends UnitTestCase {
 		
-		var $donneesDeTest = array(
-			'numero' => '31629',
-			'nom' => 'GROBIDE',
-			'race' => 'Skrim',
-			'vie' => 'entre 95 et 115',
-			'attaque' => 'entre 17 et 19',
-			'esquive' => 'entre 10 et 12',
-			'degats' => 'entre 12 et 14',
-			'regeneration' => 'entre 3 et 4',
-			'vue' => 'entre 2 et 4',
-			'armure' => 'entre 12 et 14',
-			'date_compilation' => '2006-06-25',  
-			'sortileges' => '',
-		);
-		
-		function getAbsolutePathForFile($fileName) {
-			return dirname(__FILE__). '/' . $fileName;
-		}
-		
-		function getTemporaryMySQLDumpFile() {
-			return $this->getAbsolutePathForFile('temporary_MySQL_dump_file.sql');
-		}
-
-		function getMySQLCommandLine() {
-			return 'mysql -u '._USER_.' -p'._PWD_.' -h '._HOST_.' '._DB_.' < ';
-		}
-		
-		function getMySQLTemporaryDump() {
-			return 'mysqldump -u '._USER_.' -p'._PWD_.' -h '._HOST_.' -c -t '._DB_.' > '.$this->getTemporaryMySQLDumpFile();
-		}
-		
-		function setUp() {
-			shell_exec($this->getMySQLCommandLine() . $this->getAbsolutePathForFile('donnees_initiales.sql'));
-		}
-		
-		function tearDown() {
-			shell_exec($this->getMySQLCommandLine() . $this->getAbsolutePathForFile('vidange_donnees.sql'));
-		}
-		
 		function test_ecritureTrollEnBaseSansTableau() {
 			$result = createTrollInDB('donnee non valide');
-			$this->assertError('error[createOrUpdateTrollInDB()]: input data not an array');
+			$this->assertError('error: input data is not an array');
 		} 
 		
 		function test_ecritureTrollEnBaseAvecTableauVide() {
 			$result = createTrollInDB(array());
-			$this->assertError('error[createOrUpdateTrollInDB()]: input array data is empty');
+			$this->assertError('error: input data array is empty');
 		}
 		
 		function test_ecritureTrollEnBase() {
-			createTrollInDB($this->donneesDeTest);
-			shell_exec($this->getMySQLTemporaryDump());
-			$diffCommandLine = 'diff '.$this->getAbsolutePathForFile('donnees_apres_insertion.sql').' '.$this->getTemporaryMySQLDumpFile(); 
-			$output = shell_exec($diffCommandLine);
-			$this->assertEqual('', $output);
-			shell_exec('rm ' . $this->getTemporaryMySQLDumpFile());
-		}
-		
-		function test_suppressionTrollEnBaseAvecIdentifiantInconnu() {
-			deleteTrollInDB('identifiant inconnu');
-			shell_exec($this->getMySQLTemporaryDump());
-			$diffCommandLine = 'diff '.$this->getAbsolutePathForFile('donnees_initiales.sql').' '.$this->getTemporaryMySQLDumpFile(); 
-			$output = shell_exec($diffCommandLine);
-			$this->assertEqual('', $output);
-			shell_exec('rm ' . $this->getTemporaryMySQLDumpFile());
+			$grobide = array(
+				'numero' => '31629',
+				'nom' => 'GROBIDE',
+				'race' => 'Skrim',
+				'vie' => 'entre 95 et 115',
+				'attaque' => 'entre 17 et 19',
+				'esquive' => 'entre 10 et 12',
+				'degats' => 'entre 12 et 14',
+				'regeneration' => 'entre 3 et 4',
+				'vue' => 'entre 2 et 4',
+				'armure' => 'entre 12 et 14',
+				'date_compilation' => '2006-06-25',  
+				'sortileges' => '',
+			);
+			$createQuery = getQueryForCreate($grobide);
+			$this->assertEqual(
+				"INSERT INTO `mountyhall_troll` (`numero`, `nom`, `race`, `vie`, `attaque`, `esquive`,".
+				"`degats`, `regeneration`, `vue`, `armure`, `date_compilation`, `sortileges`) VALUES (".
+				"'31629','GROBIDE','Skrim','entre 95 et 115','entre 17 et 19','entre 10 et 12','entre 12 et 14',".
+				"'entre 3 et 4','entre 2 et 4','entre 12 et 14',CURDATE(),'')", $createQuery);
 		}
 		
 		function test_suppressionTrollEnBase() {
-			deleteTrollInDB('29201');
-			shell_exec($this->getMySQLTemporaryDump());
-			$diffCommandLine = 'diff '.$this->getAbsolutePathForFile('donnees_apres_suppression.sql').' '.$this->getTemporaryMySQLDumpFile(); 
-			$output = shell_exec($diffCommandLine);
-			$this->assertEqual('', $output);
-			shell_exec('rm ' . $this->getTemporaryMySQLDumpFile());
+			$deleteQuery = getQueryForDelete('31629');
+			$this->assertEqual("DELETE FROM `mountyhall_troll` WHERE `numero` = '31629'", $deleteQuery);
 		}
 		
 		function test_ModificationTrollEnBaseSansTableau() {
 			updateTrollInDB('donnée non valide');
-			$this->assertError('error[createOrUpdateTrollInDB()]: input data not an array');
+			$this->assertError('error: input data is not an array');
 		}
 		
 		function test_ModificationTrollEnBaseAvecTableauVide() {
 			updateTrollInDB(array());
-			$this->assertError('error[createOrUpdateTrollInDB()]: input array data is empty');
+			$this->assertError('error: input data array is empty');
 		}
 		
 		function test_ModificationTrollEnBase() {
-			$donneesDeModification = array('numero'=> '29201', 'degats' => 'entre 13 et 15','regeneration' => '5');
-			updateTrollInDB($donneesDeModification);
-			shell_exec($this->getMySQLTemporaryDump());
-			$diffCommandLine = 'diff '.$this->getAbsolutePathForFile('donnees_apres_modification.sql').' '.$this->getTemporaryMySQLDumpFile(); 
-			$output = shell_exec($diffCommandLine);
-			$this->assertEqual('', $output);
-			shell_exec('rm ' . $this->getTemporaryMySQLDumpFile());
+			$donneesDeModification = array('numero'=> '31629', 'degats' => 'entre 13 et 15','regeneration' => '5');
+			$updateQuery = getQueryForUpdate($donneesDeModification);
+			$this->assertEqual(
+				"UPDATE `mountyhall_troll` SET `numero`='31629',`degats`='entre 13 et 15',`regeneration`='5',".
+				"`date_compilation`=CURDATE() WHERE `numero`='31629'", $updateQuery);
 		}
 	}
 ?>
