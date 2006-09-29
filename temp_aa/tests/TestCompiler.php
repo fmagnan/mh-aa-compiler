e37cab822d34514ed0995af51606f8753e232551
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-09-29 15:59:43 +0000

    compilation des données

---
+++ temp_aa/tests/TestCompiler.php
@@ -0,0 +1,110 @@
+<?php
+	require_once(dirname(__FILE__).'/../lib/Compiler.php');
+		
+	class TestCompiler extends UnitTestCase {
+    	
+    	function test_creationAvecMauvaiseDonnee() {
+    		$compiler = new Compiler('mauvaise entrée');
+    		$this->assertError('error[Compiler()]: wrong input data');
+    	}
+    	
+    	function test_creationAvecDonneeUnique() {
+    		$compiler = new Compiler('120');
+    		$this->assertEqual(120, $compiler->getMinimumValue());
+    		$this->assertEqual(120, $compiler->getMaximumValue());
+    	}
+    	
+    	function test_creationAvecIntervalle() {
+    		$compiler = new Compiler('entre 11 et 13');
+    		$this->assertEqual(11, $compiler->getMinimumValue());
+    		$this->assertEqual(13, $compiler->getMaximumValue());
+    	}
+    	
+    	function test_creationAvecBorneInferieure() {
+    		$compiler = new Compiler('inférieur à 3');
+    		$this->assertEqual(3, $compiler->getMinimumValue());
+    		$this->assertEqual(3, $compiler->getMaximumValue());
+    	}
+    	
+    	function test_creationAvecBorneSuperieure() {
+    		$compiler = new Compiler('supérieur à 200');
+    		$this->assertEqual(200, $compiler->getMinimumValue());
+    		$this->assertEqual(200, $compiler->getMaximumValue());
+    	}
+    	
+    	function test_nouvelleDonneeInferieureAReferenceSimple() {
+    		$compiler = new Compiler('100');
+    		$compiler->analyse('entre 80 et 90');
+    		$this->assertError('error[Compiler->analyse()]: wrong input data');
+		}
+		
+		function test_nouvelleDonneeInferieureAReferenceDouble() {
+    		$compiler = new Compiler('entre 80 et 90');
+    		$compiler->analyse('entre 60 et 70');
+    		$this->assertError('error[Compiler->analyse()]: wrong input data');
+		}
+	
+		function test_nouvelleDonneeInferieureATout() {
+    		$compiler = new Compiler('entre 5 et 6');
+    		$compiler->analyse('inférieur à 3');
+    		$this->assertError('error[Compiler->analyse()]: wrong input data');
+		}
+		
+		function test_nouvelleDonneeInferieureEtEgaleAReferenceSimple() {
+    		$compiler = new Compiler('120');
+    		$compiler->analyse('entre 110 et 120');
+    		$this->assertEqual('120', $compiler->getMinimumValue());
+    		$this->assertEqual('120', $compiler->getMaximumValue());
+		}
+		
+		function test_nouvelleDonneeSuperieureEtEgaleAReferenceSimple() {
+    		$compiler = new Compiler('120');
+    		$compiler->analyse('entre 120 et 130');
+    		$this->assertEqual('120', $compiler->getMinimumValue());
+    		$this->assertEqual('130', $compiler->getMaximumValue());
+		}
+		
+		function test_nouvelleDonneeSuperieureAReferenceSimple() {
+    		$compiler = new Compiler('120');
+    		$compiler->analyse('entre 150 et 160');
+    		$this->assertEqual('150', $compiler->getMinimumValue());
+    		$this->assertEqual('160', $compiler->getMaximumValue());
+		}
+		
+		function test_nouvelleDonneeInferieureEtEgaleAReferenceDouble() {
+    		$compiler = new Compiler('entre 120 et 140');
+    		$compiler->analyse('entre 110 et 130');
+    		$this->assertEqual('120', $compiler->getMinimumValue());
+    		$this->assertEqual('130', $compiler->getMaximumValue());
+		}
+		
+		function test_nouvelleDonneeSuperieureEtEgaleAReferenceDouble() {
+    		$compiler = new Compiler('entre 120 et 140');
+    		$compiler->analyse('entre 130 et 150');
+    		$this->assertEqual('130', $compiler->getMinimumValue());
+    		$this->assertEqual('150', $compiler->getMaximumValue());
+		}
+		
+		function test_nouvelleDonneeSuperieureAReferenceDouble() {
+    		$compiler = new Compiler('entre 120 et 140');
+    		$compiler->analyse('entre 170 et 180');
+    		$this->assertEqual('170', $compiler->getMinimumValue());
+    		$this->assertEqual('180', $compiler->getMaximumValue());
+		}
+		
+		function test_nouvelleDonneeSuperieureATout() {
+    		$compiler = new Compiler('entre 120 et 140');
+    		$compiler->analyse('supérieur à 200');
+    		$this->assertEqual('200', $compiler->getMinimumValue());
+    		$this->assertEqual('200', $compiler->getMaximumValue());
+		}
+		
+		function test_donneValeurUnique() {
+    		$compiler = new Compiler('entre 120 et 140');
+    		$compiler->analyse('entre 100 et 120');
+    		$this->assertEqual('120', $compiler->getMinimumValue());
+    		$this->assertEqual('120', $compiler->getMaximumValue());
+		}
+		
+	}
+?>
