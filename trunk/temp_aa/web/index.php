69b6b5d18c1ce387eac4abb071cf8af85780a564
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-10-02 16:25:32 +0000

    création des templates pour le site

---
+++ trunk/temp_aa/web/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// put full path to Smarty.class.php
+require(dirname(__FILE__).'/../lib/Smarty/Smarty.class.php');
+
+$smarty = new Smarty();
+
+$smarty->template_dir = dirname(__FILE__).'/smarty/templates';
+$smarty->compile_dir = dirname(__FILE__).'/smarty/templates_c';
+$smarty->cache_dir = dirname(__FILE__).'/smarty/cache';
+$smarty->config_dir = dirname(__FILE__).'/smarty/configs';
+
+$smarty->assign('name', 'Herb\'');
+$smarty->display('index.tpl');
+
+?>
