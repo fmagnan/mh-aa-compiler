42cfd048bcd8d59a0c89b443e51798989aa888b2
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-10-02 16:25:32 +0000

    création des templates pour le site

---
+++ temp_aa/web/ajoute_AA.php
@@ -0,0 +1,15 @@
+<?php
+
+// put full path to Smarty.class.php
+require(dirname(__FILE__).'/../lib/Smarty/Smarty.class.php');
+
+$smarty = new Smarty();
+
+$smarty->template_dir = dirname(__FILE__).'/smarty/templates';
+$smarty->compile_dir = dirname(__FILE__).'/smarty/templates_c';
+$smarty->cache_dir = dirname(__FILE__).'/smarty/cache';
+$smarty->config_dir = dirname(__FILE__).'/smarty/configs';
+
+$smarty->display('ajoute_AA.tpl');
+
+?>
---
+++ temp_aa/web/smarty/templates/ajoute_AA.tpl
@@ -0,0 +1,13 @@
+<html>
+	<head></head>
+	<body>
+		<form>
+			<fieldset>
+				<legend>Compilation des AAs</legend>
+				<textarea rows="10" cols="60">copier/coller ici votre AA</textarea>
+				<br />
+				<input type="submit" name="valider" value="Valider" />
+			</fieldset>
+		</form> 
+	</body>
+</html>
