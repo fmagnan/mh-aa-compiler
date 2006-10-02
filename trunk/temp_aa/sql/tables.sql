24444efed1ca94d01f456fc5a95db202ac84c8a6
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-10-02 15:10:46 +0000

    ajout du niveau

--- trunk/temp_aa/sql/tables.sql
+++ trunk/temp_aa/sql/tables.sql
@@ -1,14 +1,15 @@
 CREATE TABLE mountyhall_troll (
 	numero INT(5) NOT NULL,
 	nom VARCHAR(50) NOT NULL,
-	race ENUM('Durakuir', 'Kastar', 'Skrim', 'Tomawak') NOT NULL,
+	race ENUM('Inconnue', 'Durakuir', 'Kastar', 'Skrim', 'Tomawak') NOT NULL,
+	niveau int NOT NULL,
 	vie VARCHAR(20) NOT NULL,
 	attaque VARCHAR(20) NOT NULL,
 	esquive VARCHAR(20) NOT NULL,
 	degats VARCHAR(20) NOT NULL,
 	regeneration VARCHAR(20) NOT NULL,
-	vue VARCHAR(20) NOT NULL,
 	armure VARCHAR(20) NOT NULL,
+	vue VARCHAR(20) NOT NULL,
 	date_compilation DATE NOT NULL,
 	sortileges TINYTEXT DEFAULT '',
 	PRIMARY KEY (numero, nom, race)
