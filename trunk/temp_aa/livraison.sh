be955d39ad9db773022c88a899eb32bcf9b54a4b
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-10-12 12:05:40 +0000

    précision du script de livraison

--- trunk/temp_aa/livraison.sh
+++ trunk/temp_aa/livraison.sh
@@ -5,14 +5,29 @@ HOST=ftpperso.free.fr
 REMOTE_DIRECTORY=/mountyhall/AA/
 LOCAL_TEMPORARY_DIRECTORY=/tmp/
 
-ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/lib
-ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} lib
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/lib/*.php
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} lib/*.php
 
-ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/pub
-ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} pub
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/pub/*.txt
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} pub/*.txt
 
-ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/sql
-ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} sql
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/sql/*.sql
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} sql/*.sql
 
-ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/web
-ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} web
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/web/*.php
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} web/*.php
+
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/web/css/*.css
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} web/css/*.css
+
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/web/js/*.js
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} web/js/*.js
+
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/web/images/*.jpg
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} web/images/*.jpg
+
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/web/images/*.gif
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} web/images/*.gif
+
+ncftpget -R -DD -u ${USER} -p ${PASSWORD} ${HOST} /tmp/ ${REMOTE_DIRECTORY}/web/smarty/templates/*.tpl
+ncftpput -R -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} web/smarty/templates/*.tpl
