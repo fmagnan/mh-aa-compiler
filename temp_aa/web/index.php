c8e02570c76f8882b3fb51aa8b940276f29649a8
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-10-15 17:41:14 +0000

    ajout du formulaire d'ajout d'un sortilege

--- temp_aa/web/index.php
+++ temp_aa/web/index.php
@@ -12,18 +12,18 @@ $smarty->compile_dir = dirname(__FILE__).'/smarty/templates_c';
 $smarty->cache_dir = dirname(__FILE__).'/smarty/cache';
 $smarty->config_dir = dirname(__FILE__).'/smarty/configs';
 
-if (!array_key_exists('fieldSort', $_GET)) {
+if (!array_key_exists('fieldSort', $_REQUEST)) {
 	$fieldSort = 'numero';
 }
 else {
-	$fieldSort = $_GET['fieldSort'];
+	$fieldSort = $_REQUEST['fieldSort'];
 }
 
-if (!array_key_exists('typeSort', $_GET)) {
+if (!array_key_exists('typeSort', $_REQUEST)) {
 	$typeSort = 'ASC';
 }
 else {
-	$typeSort = $_GET['typeSort'];
+	$typeSort = $_REQUEST['typeSort'];
 }
 
 $tousLesTrolls = getTousLesTrolls($fieldSort, $typeSort);
@@ -31,15 +31,28 @@ $tousLesTrolls = getTousLesTrolls($fieldSort, $typeSort);
 $smarty->assign('typeSort', $typeSort);
 $smarty->assign('fieldSort', $fieldSort);
 $smarty->assign('trolls', $tousLesTrolls);
-$smarty->assign('nombreDeLignes', count($tousLesTrolls));
+$smarty->assign('sortileges', $SORTILEGES);
 
-if (array_key_exists('id', $_GET)) {
-	$id = $_GET['id'];
-	$infosTrollFromDB = getInfosTrollFromDB(intval($id));
+if (array_key_exists('id', $_REQUEST)) {
+	$id = $_REQUEST['id'];
+	$intvalId = intval($id);
+	if (array_key_exists('ajout_sortilege', $_REQUEST)) {
+		$sortilegeAAjouter = $_REQUEST['ajout_sortilege'];
+		addKnownSpell($intvalId, $sortilegeAAjouter);
+	}
+	$infosTrollFromDB = getInfosTrollFromDB($intvalId);
 	$smarty->assign('id', $id);
 	$smarty->assign('ficheTroll', $infosTrollFromDB);
 	$ageAnalyse = getAgeAnalyse($infosTrollFromDB['date_compilation']);
 	$smarty->assign('ageAnalyse', $ageAnalyse);
+	$listeSortileges = getSpellsList($intvalId);
+	if ('' == $listeSortileges) {
+		$smarty->assign('aucunSortConnu', 'aucun sort connu');
+	}
+	else {
+		$sortilegesConnus = explode(';', $listeSortileges);
+		$smarty->assign('sortilegesConnus', $sortilegesConnus);
+	}
 }
 
 $smarty->display('main_template.tpl');
