8faa3552c751bef8f19fdb4914859cf2bc233775
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-10-25 16:06:58 +0000

    prise en compte de la maintenance du site lors de la livraison des fichiers par FTP

---
+++ temp_aa/web/restartSiteAfterMaintenance.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__FILE__) . '/../lib/maintenance.inc.php';
+
+$activationCode = $_GET['activation_code'];
+if ($activationCode == 'doubleZero') {
+	restartSiteAfterMaintenance();
+}
+
+
+?>
