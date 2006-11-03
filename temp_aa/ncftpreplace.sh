a442d252a4d266d18fbf574a739f2bd26a9ade10
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2006-11-03 10:21:53 +0000

    changement d'hebergement

--- temp_aa/ncftpreplace.sh
+++ temp_aa/ncftpreplace.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-USER=sirherbert
-HOST=ftpperso.free.fr
+USER=admin@sirherbert.byethost32.com
+HOST=206.222.15.234
 PASSWORD=$1
 LOCAL_FILES=$2
 REMOTE_DIRECTORY=$3
 
 for i in `ls $2`
 do
-	ncftpput -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} ${i}
+	ncftpput -m -u ${USER} -p ${PASSWORD} ${HOST} ${REMOTE_DIRECTORY} ${i}
 done
