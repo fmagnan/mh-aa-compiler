81f377fce645e1a6838467bcd9840df3a31472d9
fmagnan <fmagnan@b204db90-7f1e-0410-95e3-9b96aca577d3>
2007-07-13 16:42:26 +0000

    modification de la configuration du script de deploiement

--- trunk/temp_aa/etc/config.inc.php
+++ trunk/temp_aa/etc/config.inc.php
@@ -4,5 +4,13 @@
 	define('_USER_', 'pbem');
 	define('_PWD_', 'pbem');
 	define ('_DB_', 'pbem');
+	
+	$ftpConfig = array(
+		'ftpPassword' => '',
+		'httpUser' => '',
+		'httpPassword' => '',
+		'host' => 'http://slx0.dyndns.org/AA/web/',
+		'remoteRootDirectory' => '/AA/',
+	);
 
 ?>
